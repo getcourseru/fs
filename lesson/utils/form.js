92176eb74aea8015c85416c1a9afcd4c824605e4
undefined <98763819+getcourseru@users.noreply.github.com>
2022-10-15 17:39:55 +0300

    Update form.js

--- lesson/utils/form.js
+++ lesson/utils/form.js
@@ -8,16 +8,10 @@ export function renderForm(price, options) {
     const $container = document.querySelector('.form-content .builder');
     const $btn = $container.querySelector('.f-btn');
 
-    const packageKindTitle = packageKind === `pack`
-        ? `пакет (не ограничен временными сроками)` 
-        : lessonCount > 16
-            ?   `абонемент на 3 месяца`
-            :   `абонемент на 1 месяц`;
-
     $container.insertAdjacentHTML('afterbegin', 
         `<div class='prod'>
             <p class='title'>${packageKind !== `group` ? `Уроки с преподавателем` : `Уроки с преподавателем в группе`}</p>
-            <p class='info'>Тип: ${packageKindTitle}</p>
+            <p class='info'>Тип: ${packageKind === `pack` ? `пакет (не ограничен временными сроками)` : lessonCount > 16 ? `абонемент на 3 месяца` : `абонемент на 1 месяц`}</p>
             <p class='info'>Преподаватель: ${teacherKind === `ru` ? `русскоязычный` : `носитель`}</p> 
             <p class='info'>Количество уроков: ${lessonCount}</p>
             <p class='info'>Продолжительность урока: ${lessonDuration} минут</p>
