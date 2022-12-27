0d7762ef209ddf908958671bf6acc0482443c715
undefined <98763819+getcourseru@users.noreply.github.com>
2022-12-27 19:11:06 +0300

    Update index.js

--- lesson/index.js
+++ lesson/index.js
@@ -1,13 +1,3 @@
-/*import { parseURL } from "./utils/url.js";
-import { getPrice } from "./utils/price.js";
-import { renderForm } from "./utils/form.js";
-const options = parseURL();
-
-fetch(`https://getcourseru.github.io/fs/lesson/prices.json`)
-    .then((response) => response.json())
-    .then((data) => getPrice(data, options))
-    .then((price) => renderForm(price, options))*/
-
 fetch(`https://getcourseru.github.io/fs/lesson/prices.json`)
     .then((response) => response.json())
     .then((data) => getPrice(data))
