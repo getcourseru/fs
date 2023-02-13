8982ba1ec47532763a927f08c2dcbf2016bbc6ab
undefined <98763819+getcourseru@users.noreply.github.com>
2023-02-13 18:25:47 +0300

    Delete lesson directory

--- lesson/utils/placeholder.js
+++
@@ -1,8 +0,0 @@
-export function addPlaceholder() {
-    const [nameInput, ] = document.getElementsByName('formParams[full_name]');
-    nameInput.placeholder = `Имя`;
-    const [emailInput, ] = document.getElementsByName('formParams[email]');
-    emailInput.placeholder = `Email`;
-    const [phoneInput, ] = document.getElementsByName('formParams[phone]');
-    phoneInput.placeholder = `Телефон`;
-}
